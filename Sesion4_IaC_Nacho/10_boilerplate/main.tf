c7009ed633413b6fc0ff65148dc312cd239c55cf
Nacho Carretero <nacho@icarretero.com>
2023-11-03 14:27:29 +0100

    Add Session 4 boilerplate and exercises

---
+++ Sesion4_IaC_Nacho/10_boilerplate/main.tf
@@ -0,0 +1,26 @@
+data "aws_ami" "bitnami_nginx" {
+  most_recent = true
+  owners      = ["979382823631"] # Bitnami
+
+  filter {
+    name   = "name"
+    values = ["bitnami-nginx-1.23.3-7*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "web_server" {
+  ami                         = data.aws_ami.bitnami_nginx.id
+  instance_type               = "t3.micro"
+  subnet_id                   = data.aws_subnet.selected.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [data.aws_security_group.allow_traffic.id]
+
+  tags = {
+    Name = "web_server"
+  }
+}
